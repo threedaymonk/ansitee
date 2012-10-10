a13e8dfebecd2ee05308f393eef05642344b3fd6
Paul Battley <pbattley@gmail.com>
2012-10-10 22:02:17 +0100

    Tabs to spaces

--- ansitee.c
+++ ansitee.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 1988, 1993
- *	The Regents of the University of California.  All rights reserved.
+ *    The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,12 +30,12 @@
 #ifndef lint
 static const char copyright[] =
 "@(#) Copyright (c) 1988, 1993\n\
-	The Regents of the University of California.  All rights reserved.\n";
+    The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
 #if 0
-static char sccsid[] = "@(#)tee.c	8.1 (Berkeley) 6/6/93";
+static char sccsid[] = "@(#)tee.c    8.1 (Berkeley) 6/6/93";
 #endif
 static const char rcsid[] =
   "$FreeBSD$";
@@ -52,9 +52,9 @@ static const char rcsid[] =
 #include <unistd.h>
 
 typedef struct _list {
-	struct _list *next;
-	int fd;
-	const char *name;
+    struct _list *next;
+    int fd;
+    const char *name;
 } LIST;
 static LIST *head;
 
@@ -64,76 +64,76 @@ static void usage(void);
 int
 main(int argc, char *argv[])
 {
-	LIST *p;
-	int n, fd, rval, wval;
-	char *bp;
-	int append, ch, exitval;
-	char *buf;
-#define	BSIZE (8 * 1024)
+    LIST *p;
+    int n, fd, rval, wval;
+    char *bp;
+    int append, ch, exitval;
+    char *buf;
+#define BSIZE (8 * 1024)
 
-	append = 0;
-	while ((ch = getopt(argc, argv, "ai")) != -1)
-		switch((char)ch) {
-		case 'a':
-			append = 1;
-			break;
-		case 'i':
-			(void)signal(SIGINT, SIG_IGN);
-			break;
-		case '?':
-		default:
-			usage();
-		}
-	argv += optind;
-	argc -= optind;
+    append = 0;
+    while ((ch = getopt(argc, argv, "ai")) != -1)
+        switch((char)ch) {
+        case 'a':
+            append = 1;
+            break;
+        case 'i':
+            (void)signal(SIGINT, SIG_IGN);
+            break;
+        case '?':
+        default:
+            usage();
+        }
+    argv += optind;
+    argc -= optind;
 
-	if ((buf = malloc(BSIZE)) == NULL)
-		err(1, "malloc");
+    if ((buf = malloc(BSIZE)) == NULL)
+        err(1, "malloc");
 
-	add(STDOUT_FILENO, "stdout");
+    add(STDOUT_FILENO, "stdout");
 
-	for (exitval = 0; *argv; ++argv)
-		if ((fd = open(*argv, append ? O_WRONLY|O_CREAT|O_APPEND :
-		    O_WRONLY|O_CREAT|O_TRUNC, DEFFILEMODE)) < 0) {
-			warn("%s", *argv);
-			exitval = 1;
-		} else
-			add(fd, *argv);
+    for (exitval = 0; *argv; ++argv)
+        if ((fd = open(*argv, append ? O_WRONLY|O_CREAT|O_APPEND :
+            O_WRONLY|O_CREAT|O_TRUNC, DEFFILEMODE)) < 0) {
+            warn("%s", *argv);
+            exitval = 1;
+        } else
+            add(fd, *argv);
 
-	while ((rval = read(STDIN_FILENO, buf, BSIZE)) > 0)
-		for (p = head; p; p = p->next) {
-			n = rval;
-			bp = buf;
-			do {
-				if ((wval = write(p->fd, bp, n)) == -1) {
-					warn("%s", p->name);
-					exitval = 1;
-					break;
-				}
-				bp += wval;
-			} while (n -= wval);
-		}
-	if (rval < 0)
-		err(1, "read");
-	exit(exitval);
+    while ((rval = read(STDIN_FILENO, buf, BSIZE)) > 0)
+        for (p = head; p; p = p->next) {
+            n = rval;
+            bp = buf;
+            do {
+                if ((wval = write(p->fd, bp, n)) == -1) {
+                    warn("%s", p->name);
+                    exitval = 1;
+                    break;
+                }
+                bp += wval;
+            } while (n -= wval);
+        }
+    if (rval < 0)
+        err(1, "read");
+    exit(exitval);
 }
 
 static void
 usage(void)
 {
-	(void)fprintf(stderr, "usage: tee [-ai] [file ...]\n");
-	exit(1);
+    (void)fprintf(stderr, "usage: tee [-ai] [file ...]\n");
+    exit(1);
 }
 
 static void
 add(int fd, const char *name)
 {
-	LIST *p;
+    LIST *p;
 
-	if ((p = malloc(sizeof(LIST))) == NULL)
-		err(1, "malloc");
-	p->fd = fd;
-	p->name = name;
-	p->next = head;
-	head = p;
+    if ((p = malloc(sizeof(LIST))) == NULL)
+        err(1, "malloc");
+    p->fd = fd;
+    p->name = name;
+    p->next = head;
+    head = p;
 }
